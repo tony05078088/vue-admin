bd4705915845a50d53362533fad95d7a2f65774b
William <tony05078088@gmail.com>
2022-02-20 15:55:55 +0800

    feat(breadCrumb):  動畫處理

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,13 +1,17 @@
 <template>
     <el-breadcrumb class="breadcrumb" separator="/">
-        <el-breadcrumb-item v-for="(item, index) in BreadCrumbData" :key="item.path">
-            <!-- 不可點擊 -->
-            <span class="no-redirect" v-if="index === BreadCrumbData.length - 1">
-                {{ item.meta.title }}
-            </span>
-            <!-- 可點擊 -->
-            <span class="redirect" v-else @click="onLinkClick(item)"> {{ item.meta.title }} </span>
-        </el-breadcrumb-item>
+        <transition-group name="breadcrumb">
+            <el-breadcrumb-item v-for="(item, index) in BreadCrumbData" :key="item.path">
+                <!-- 不可點擊 -->
+                <span class="no-redirect" v-if="index === BreadCrumbData.length - 1">
+                    {{ item.meta.title }}
+                </span>
+                <!-- 可點擊 -->
+                <span class="redirect" v-else @click="onLinkClick(item)">
+                    {{ item.meta.title }}
+                </span>
+            </el-breadcrumb-item>
+        </transition-group>
     </el-breadcrumb>
 </template>
 
