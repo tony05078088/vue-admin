e924c76fd1d48bc5e78d2042fa20866eb4e72a74
William <tony05078088@gmail.com>
2022-04-04 13:21:25 +0800

    feat(excel): 打api傳遞解析後的excel數據

--- src/api/user-manage.js
+++ src/api/user-manage.js
@@ -8,3 +8,12 @@ export const getUserManageList = data => {
         data
     });
 };
+
+// 新增員工(excel導入)
+export const getUserBatchImport = data => {
+    return request({
+        url: '/user-manage/batch/import',
+        method: 'POST',
+        data
+    });
+};
--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -4,9 +4,35 @@
 
 <script setup>
 import UploadExcel from '@/components/UploadExcel';
+import { USER_RELATIONS } from './utils';
+import { ElMessage } from 'element-plus';
+import { getUserBatchImport } from '@/api/user-manage';
+import { useI18n } from 'vue-i18n';
+import { useRouter } from 'vue-router';
 
-const onSuccess = excelData => {
-    console.log(excelData);
+const i18n = useI18n();
+/* eslint-disable */
+// 數據解析成功之後的callback
+
+const router = useRouter();
+const onSuccess = async ({ header, result }) => {
+    const updateData = generateData(result);
+    await getUserBatchImport(updateData);
+    ElMessage.success(result.length + i18n.t('msg.excel.importSuccess'));
+    router.push('/user/manage');
+};
+
+// 篩選數據
+const generateData = results => {
+    const arr = [];
+    results.forEach(item => {
+        const userInfo = {};
+        Object.keys(item).forEach(key => {
+            userInfo[USER_RELATIONS[key]] = item[key];
+        });
+        arr.push(userInfo);
+    });
+    return arr;
 };
 </script>
 
