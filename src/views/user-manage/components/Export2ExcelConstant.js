3826eae467d80836c80c755b06b12116827eec2e
William <tony05078088@gmail.com>
2022-04-05 21:33:41 +0800

    feat(excel): json數據輸出為excel檔

---
+++ src/views/user-manage/components/Export2ExcelConstant.js
@@ -0,0 +1,6 @@
+export const USER_RELATIONS = {
+    姓名: 'username',
+    角色: 'role',
+    联系方式: 'mobile',
+    开通时间: 'openTime'
+};
--- src/views/user-manage/components/Exprt2Excel.vue
+++ src/views/user-manage/components/Exprt2Excel.vue
@@ -19,6 +19,7 @@ import { defineProps, defineEmits, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { watchSwitchLang } from '@/utils/i18n';
 import { getUserManageAllList } from '@/api/user-manage';
+import { USER_RELATIONS } from './Export2ExcelConstant';
 defineProps({
     modelValue: {
         type: Boolean,
@@ -46,9 +47,40 @@ const closed = () => {
 
 const onConfirm = async () => {
     loading.value = true;
+    // 數據
     const allUser = (await getUserManageAllList()).list;
     console.log(allUser);
+
+    // 導入工具包
+    const excel = await import('@/utils/Export2Excel');
+    const data = formateJson(USER_RELATIONS, allUser);
+    excel.export_json_to_excel({
+        // excel表頭 ['姓名', '聯繫方式', '角色', '入職時間']
+        header: Object.keys(USER_RELATIONS),
+        // excel數據
+        data,
+        // 文件名稱
+        filename: excelName.value || exportDefaultName
+        // 是否自動列寬
+        // 文件類型
+    });
     closed();
+};
+
+//  當使用excel_json_to_excel時,傳遞的數據 必須是二維array: [[關羽,軍團長,15151515,2021-12-31],[關羽,軍團長,15151515,2021-12-31],[關羽,軍團長,15151515,2021-12-31]]
+const formateJson = (headers, rows) => {
+    // header : 對照表 , rows: 數據源 第一個代表數錊,第二個代表裡面的數據內容
+    return rows.map(item => {
+        return Object.keys(headers).map(key => {
+            // 角色需要進行特殊處例
+            if (headers[key] === 'role') {
+                const roles = item[headers[key]];
+                return JSON.stringify(roles.map(role => role.title));
+            }
+
+            return item[headers[key]];
+        });
+    });
 };
 </script>
 
