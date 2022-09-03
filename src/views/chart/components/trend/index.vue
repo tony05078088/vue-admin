1fcf89ee9a467dbb7c75e4a7ae640517cbc99628
William <tony05078088@gmail.com>
2022-09-03 11:51:14 +0800

    feat(dataVisualize): 累計收益-moudle劃分

--- src/views/chart/components/trend/index.vue
+++ src/views/chart/components/trend/index.vue
@@ -1,8 +1,22 @@
 <template>
-    <div class="container">累計收益趨勢圖</div>
+    <el-card class="container">
+        <el-row :gutter="32">
+            <el-col :span="6">
+                <!-- 數據部分 -->
+                <trend-data />
+            </el-col>
+            <el-col :span="18">
+                <!-- 圖表部分 -->
+                <trend-chart />
+            </el-col>
+        </el-row>
+    </el-card>
 </template>
 
-<script setup></script>
+<script setup>
+import trendChart from './trendChart.vue';
+import trendData from './trendData.vue';
+</script>
 
 <style scoped lang="scss">
 .container {
---
+++ src/views/chart/components/trend/trendChart.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>圖表部分</div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="scss"></style>
